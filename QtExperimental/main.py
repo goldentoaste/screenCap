c151fdc5526f0471605acd59d8e87dad1f9467e9
Goldentoaste <rayg2375@gmail.com>
2021-07-19 01:14:57 -0700

    init ui

--- QtExperimental/main.py
+++ QtExperimental/main.py
@@ -1,239 +1,314 @@
 import os
 from ConfigManager import ConfigManager
-from os import path, getenv
+from os import path, getenv, remove, mkdir
 from PyQt5.QtGui import QIntValidator
 from PyQt5.QtWidgets import *
 import sys
+
 configDir = path.join(getenv("appdata"), "screenCap")
 configFile = path.join(configDir, "config.ini")
 
 from values import defaultVariables
-from PyQt5.QtCore import QFile, Qt
+from PyQt5.QtCore import Qt
+from win32com.client import Dispatch
+import pythoncom
+import ctypes
+
+
+def resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = path.abspath(".")
+    return path.join(base_path, relative_path)
+
+
+executable = "screenCap.exe"
+iconName = "icon.ico"
+configDir = path.join(getenv("appdata"), "screenCap")
+configFile = path.join(configDir, "config.ini")
+singletonFile = path.join(configDir, "singleton.lock")
+shortCutDest = path.join(
+    getenv("appdata"), "Microsoft\Windows\Start Menu\Programs\Startup"
+)
+shortCutFile = path.join(shortCutDest, "screenCap.lnk")
+shortCutTarget = path.join(
+    resource_path(path.dirname(path.abspath(__file__))), executable
+)
+
+
 class Main(QWidget):
-    
     def __init__(self):
         super().__init__()
         self.initConfig()
         self.initGUI()
-        
-    
+
     def initConfig(self):
-        self.config = ConfigManager('D:\Python Project\screenCap\QtExperimental\config.ini', defaultVariables)
-    
+        self.config = ConfigManager(
+            "D:\Python Project\screenCap\QtExperimental\config.ini", defaultVariables
+        )
+
+    def closeEvent(self, a0) -> None:
+        if self.config.iminx:
+            print("closing")
+            a0.ignore()
+            return
+        return super().closeEvent(a0)
+
+    def hideEvent(self, a0) -> None:
+        if self.config.imintray:
+            print("closing")
+            a0.ignore()
+            return
+        return super().hideEvent(a0)
+
     def initGUI(self):
         self.setWindowTitle("screenCap owo")
         self.setFocusPolicy(Qt.FocusPolicy.ClickFocus)
-        
+
         self.tabs = QTabWidget(self)
         self.generaltab = QWidget()
         self.hotkeystab = QWidget()
         self.generaltab.layout = QVBoxLayout()
-        
+
         self.generaltab.setLayout(self.generaltab.layout)
         self.tabs.addTab(self.generaltab, "General")
         self.tabs.addTab(self.hotkeystab, "Hotkeys")
-        
-        
-        #first row
+
+        # first row
         temphbox = QHBoxLayout()
-        #start up options
-        startupGroup = QGroupBox('Start up options')
+        # start up options
+        startupGroup = QGroupBox("Start up options")
         temphbox.addWidget(startupGroup)
-        
+
         startupLayout = QVBoxLayout()
-        self.startUpCheck = QCheckBox('Run on Start up')
-        self.startminCheck = QCheckBox('Start minimalized')
+        self.startUpCheck = QCheckBox("Run on Start up")
+        self.startminCheck = QCheckBox("Start minimalized")
         startupLayout.addWidget(self.startUpCheck)
         startupLayout.addWidget(self.startminCheck)
         startupGroup.setLayout(startupLayout)
-        
-        #controls
-        miscGroup = QGroupBox('System Tray')
+
+        # controls
+        miscGroup = QGroupBox("System Tray")
         temphbox.addWidget(miscGroup)
         misclayout = QVBoxLayout()
         miscGroup.setLayout(misclayout)
 
-        self.minTrayCheck = QCheckBox('Minimize to system tray')
-        self.xTrayCheck = QCheckBox('Minimize to tray when \'X\' is pressed')
-        
+        self.minTrayCheck = QCheckBox("Minimize to system tray")
+        self.xTrayCheck = QCheckBox("Minimize to tray when 'X' is pressed")
+
         misclayout.addWidget(self.minTrayCheck)
         misclayout.addWidget(self.xTrayCheck)
-        
-        #second row (recycler stuff)
-        recycleGroup = QGroupBox('Recycle Bin')
-        self.showRecycleButton = QPushButton('Show recycle bin')
-        self.clearRecycleButton = QPushButton('Clear recycle bin')
+
+        # second row (recycler stuff)
+        recycleGroup = QGroupBox("Recycle Bin")
+        self.showRecycleButton = QPushButton("Show recycle bin")
+        self.clearRecycleButton = QPushButton("Clear recycle bin")
         self.recycleCapacityEdit = CustomLineEdit()
         self.recycleCapacityEdit.setValidator(QIntValidator(1, 200))
-        self.recycleCheck = QCheckBox('Use recycle bin')
-        
+        self.recycleCheck = QCheckBox("Use recycle bin")
+
         recycleMainLayout = QHBoxLayout()
         recycleGroup.setLayout(recycleMainLayout)
-        
+
         recyclelayout1 = QVBoxLayout()
         recyclelayout2 = QVBoxLayout()
         recycleButtonGroup = QHBoxLayout()
-        
+
         recyclelayout1.addWidget(self.recycleCheck)
-        recyclelayout1.addWidget(QLabel('Recycle bin capacity'))
-        
+        recyclelayout1.addWidget(QLabel("Recycle bin capacity"))
+
         recycleButtonGroup.addWidget(self.showRecycleButton)
         recycleButtonGroup.addWidget(self.clearRecycleButton)
-        
+
         recyclelayout2.addLayout(recycleButtonGroup)
         recyclelayout2.addWidget(self.recycleCapacityEdit)
-        
-        
+
         recycleMainLayout.addLayout(recyclelayout1)
         recycleMainLayout.addLayout(recyclelayout2)
-        
-        
-    
-        #third row, saving stuff
-        savingGroup = QGroupBox('Save Options')
+
+        # third row, saving stuff
+        savingGroup = QGroupBox("Save Options")
         mainsavingLayout = QVBoxLayout()
         savingGroup.setLayout(mainsavingLayout)
-        
-        self.savingOption = QComboBox() #scaled image, original, scaled with drawing, original with drawing.
-        self.savePromptCheck = QCheckBox('Alwasys show prompt')
+
+        self.savingOption = (
+            QComboBox()
+        )  # scaled image, original, scaled with drawing, original with drawing.
+        self.savePromptCheck = QCheckBox("Alwasys show prompt")
         self.defaultSaveLocation = CustomLineEdit()
-        self.useLastLocationCheck = QCheckBox('Use last save location')
-            
+        self.useLastLocationCheck = QCheckBox("Use last save location")
+
         savelayoutUp = QHBoxLayout()
-        
-        
+
         savelayoutLeft = QVBoxLayout()
-        savelayoutLeft.addWidget(QLabel('Default save options'))
-        savelayoutLeft.addWidget(QLabel('Default save location'))
-        
+        savelayoutLeft.addWidget(QLabel("Default save options"))
+        savelayoutLeft.addWidget(QLabel("Default save location"))
+
         saveRight = QVBoxLayout()
         saveUpRight = QHBoxLayout()
         saveUpRight.addWidget(self.savingOption)
         saveUpRight.addWidget(self.savePromptCheck)
-        
+
         saveRight.addLayout(saveUpRight)
         saveRight.addWidget(self.defaultSaveLocation)
-        
-        
+
         savelayoutUp.addLayout(savelayoutLeft)
         savelayoutUp.addLayout(saveRight)
         mainsavingLayout.addLayout(savelayoutUp)
         mainsavingLayout.addWidget(self.useLastLocationCheck)
 
-        
-        
-        #bottom
+        # bottom
         self.generaltab.layout.addLayout(temphbox)
         self.generaltab.layout.addWidget(recycleGroup)
         self.generaltab.layout.addWidget(savingGroup)
-        self.captureButton = QPushButton('Capture')
-    
+        self.captureButton = QPushButton("Capture")
+
         self.generaltab.layout.addWidget(self.captureButton)
+        self.applyGeneralConfig()
         self.setupGeneralTab()
-        
+
         self.resize(self.tabs.sizeHint())
-    
-        
+
         self.show()
-    
-    
-    def setupGeneralTab(self):
+
+    def applyGeneralConfig(self):
         
+        self.startUpCheck.setChecked(bool(self.config.istartup))
+        self.startminCheck.setChecked(bool(self.config.istartmin))
+        self.minTrayCheck.setChecked(bool(self.config.imintray))
+        self.xTrayCheck.setChecked(bool(self.config.iminx))
+        self.recycleCheck.setChecked(bool(self.config.iuserecycle))
+        self.recycleCapacityEdit.setText(str(self.config.irecyclecapacity))
+        self.savingOption.addItems(['Scaled', 'Scaled with drawings', 'Original', 'Original with drawings'])
+        self.savingOption.setCurrentIndex(self.config.isaveoption)
+        self.savePromptCheck.setChecked(bool(self.config.ishowsaveprompt))
+        self.defaultSaveLocation.setText(self.config.ssavelocation)
+        self.useLastLocationCheck.setChecked(bool(self.config.iuselastsave))
+
+    def updateDidsabled(self):
+        self.showRecycleButton.setEnabled(self.recycleCheck.isChecked())
+        self.clearRecycleButton.setEnabled(self.recycleCheck.isChecked())
+        self.recycleCapacityEdit.setEnabled(self.recycleCheck.isChecked())
+        self.savingOption.setDisabled(self.savePromptCheck.isChecked())
+        self.defaultSaveLocation.setDisabled(self.useLastLocationCheck.isChecked())
+
+    def setupGeneralTab(self):
         def connectck(check, func):
             check.stateChanged.connect(lambda i: func())
-        
+
         def startUp():
-            
             self.config.istartup = int(self.startUpCheck.isChecked())
-            #TODO implement start up behaviour
-            
+            # TODO implement start up behaviour
+
+            if self.startUpCheck.isChecked():
+                pythoncom.CoInitialize()
+                shell = Dispatch("WScript.Shell")
+                st = shell.CreateShortCut(shortCutFile)
+                st.Targetpath = shortCutTarget
+                st.save()
+            else:
+                if path.isfile(shortCutFile):
+                    remove(shortCutFile)
+
         self.startUpCheck.stateChanged.connect(lambda i: startUp())
-        
+
         def startMin():
-            
             self.config.istartup = int(self.startminCheck.isChecked())
-            #TODO implement start up behaviour
-            
+            # TODO implement start up behaviour
+
         self.startminCheck.stateChanged.connect(lambda i: startMin())
-        
+
         def minToTray():
             self.config.imintray = int(self.minTrayCheck.isChecked())
+
         connectck(self.minTrayCheck, minToTray)
-        
+
         def minX():
             self.config.iminx = int(self.xTrayCheck.isChecked())
+
         connectck(self.xTrayCheck, minX)
 
         def useRecycle():
-            self.config.iuserecycle =  int(self.recycleCheck.isChecked())
+            self.config.iuserecycle = int(self.recycleCheck.isChecked())
+            self.updateDisabled()
+
         connectck(self.recycleCheck, useRecycle)
-        
+
         def showRecycle():
-            print('show recycle clicked')
-            #TODO implement recycle menu
-        
+            print("show recycle clicked")
+            # TODO implement recycle menu
+
         self.showRecycleButton.clicked.connect(lambda: showRecycle())
-        
+
         def clearRecycle():
-            print('clear recycle cliked')
-            #TODO implement recycle menu
+            print("clear recycle cliked")
+            # TODO implement recycle menu
+
         self.clearRecycleButton.clicked.connect(lambda: clearRecycle())
-        
-        
+
         def capacityChange():
-            if self.recycleCapacityEdit.text() == '':
-                self.recycleCapacityEdit.setText('0')
-            print('capacityChange',int(self.recycleCapacityEdit.text()))
-            
+            if self.recycleCapacityEdit.text() == "":
+                self.recycleCapacityEdit.setText("0")
+            print("capacityChange", int(self.recycleCapacityEdit.text()))
+
         self.recycleCapacityEdit.lostFocus = capacityChange
 
         def saveOption(val):
             self.config.isaveoption = val
-        
+
         self.savingOption.currentIndexChanged.connect(lambda i: saveOption(i))
-        
+
         def alwaysShowPrompt():
             self.config.ishowsaveprompt = int(self.savePromptCheck.isChecked())
-            self.savingOption.setDisabled(self.savePromptCheck.isChecked())
-            
+            self.updateDidsabled()
+
         connectck(self.savePromptCheck, alwaysShowPrompt)
-        
+
         def getSaveLocation():
-            path, _ = QFileDialog.getExistingDirectory(self, 'Choose default save location', os.getenv('HOME'))
+            path = QFileDialog.getExistingDirectory(
+                self, "Choose default save location", os.getenv("HOME")
+            )
             if path:
                 self.config.ssavelocation = path
+                self.defaultSaveLocation.setText(path)
                 print(path)
 
         def checkValidPath():
             if not os.path.exists(self.defaultSaveLocation.text()):
                 self.defaultSaveLocation.setText(self.config.ssavelocation)
-                
-        self.defaultSaveLocation.onFocus = getSaveLocation
+
+        self.defaultSaveLocation.onclick = getSaveLocation
         self.defaultSaveLocation.lostFocus = checkValidPath
-        
+
         def useLastLocation():
             self.config.iuselastsave = int(self.useLastLocationCheck.isChecked())
-            self.defaultSaveLocation.setDisabled(self.useLastLocationCheck.isChecked())
-        
+            self.updateDidsabled()
+
         connectck(self.useLastLocationCheck, useLastLocation)
-        
-        
-        
+
 
 class CustomLineEdit(QLineEdit):
-    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.onFocus = lambda: ()
         self.lostFocus = lambda: ()
+        self.onclick = lambda : ()
 
     def focusInEvent(self, a0) -> None:
         self.onFocus()
         return super().focusInEvent(a0)
-    
+
     def focusOutEvent(self, a0) -> None:
         self.lostFocus()
         return super().focusOutEvent(a0)
+    
+    def mousePressEvent(self, a0) -> None:
+        self.onclick()
+        return super().mousePressEvent(a0)
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = Main()
